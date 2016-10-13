9fb0f7b0399fb580668141b5e2ba6f6b37d98923
Zlata Son <filingame12@gmail.com>
2016-10-13 03:22:40 +0300

    commit

--- lesson_1/js/main.js
+++ lesson_1/js/main.js
@@ -1,2 +1,10 @@
+
+// Join method
 var colors = ["Green", "Orange", "Blue"];
 document.getElementById("join").innerHTML = colors.toString();
+
+// Join method
+var colors = ["Black", "Yellow", "Orange"];
+colors.shift("Orange");
+colors.unshift("Lime");
+document.getElementById("shift-unshift").innerHTML = colors.toString();
