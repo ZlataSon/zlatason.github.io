e592d8874e7f900704feea27f18069129a2de91d
Zlata Son <filingame12@gmail.com>
2016-10-13 12:41:21 +0300

    commit

--- lesson_1/js/main.js
+++ lesson_1/js/main.js
@@ -4,7 +4,12 @@ var colors = ["Green", "Orange", "Blue"];
 document.getElementById("join").innerHTML = colors.join(" *** ");
 
 // Shift & Unshift methods
-var color = ["Black", "Yellow", "Orange"];
+var color = ["Black", "Yellow", "Red"];
 color.shift();
 color.unshift("Lime");
-document.getElementById("shift-unshift").innerHTML = color.toString();
+document.getElementById("shift-unshift").innerHTML = color;
+
+// Splice method
+var mainColor = ["Coral", "Aqua", "Gray", "Pink"];
+mainColor.splice(1, 2, "Red", "Fuchsia");
+document.getElementById("splice").innerHTML = mainColor;
