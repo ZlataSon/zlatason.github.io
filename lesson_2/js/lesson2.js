3bbc125ffff165df697863990bf1d6ebecec9524
Zlata Son <filingame12@gmail.com>
2016-10-23 00:22:47 +0300

    commit lesson 2 js

--- lesson_2/js/lesson2.js
+++ lesson_2/js/lesson2.js
@@ -1,3 +1,67 @@
-/**
- * Created by lime on 18.10.16.
- */
+ArrayObject = {};
+
+ArrayObject.pop = function () {
+    var objectLength = this.length-1;
+    element = this[objectLength];
+    delete this[objectLength];
+    this.length = objectLength;
+    return element;
+};
+
+ArrayObject.push = function () {
+
+};
+
+ArrayObject.slice = function () {
+
+};
+
+ArrayObject.join = function () {
+
+};
+
+ArrayObject.reverse = function () {
+
+};
+
+var arrayMethodPop = ['a', 'b', 'c', 4, 5, 6];
+var arrayMethodPush = ['a', 'b', 'c', 4, 5, 6];
+var arrayMethodSlice = ['a', 'b', 'c', 4, 5, 6];
+var arrayMethodJoin = ['a', 'b', 'c', 4, 5, 6];
+var arrayMethodReverse = ['a', 'b', 'c', 4, 5, 6];
+
+console.log("Method Pop");
+console.log(ArrayObject.pop.call(arrayMethodPop));
+console.log(arrayMethodPop);
+Array.prototype.pop = ArrayObject.pop;
+console.log(arrayMethodPop.pop());
+console.log(arrayMethodPop);
+
+console.log("Method Push");
+console.log(ArrayObject.push.call(arrayMethodPush, 7));
+console.log(arrayMethodPush);
+Array.prototype.push = ArrayObject.push;
+console.log(arrayMethodPush.push(8));
+console.log(arrayMethodPush);
+
+console.log("Method Slice");
+console.log(ArrayObject.slice.call(arrayMethodSlice, 1, 3));
+console.log(ArrayObject.slice.call(arrayMethodSlice, 2));
+console.log(ArrayObject.slice.call(arrayMethodSlice, -2));
+console.log(arrayMethodSlice);
+Array.prototype.slice = ArrayObject.slice;
+console.log(arrayMethodSlice.slice(1, 3));
+
+console.log("Method Join");
+console.log(ArrayObject.join.call(arrayMethodJoin, "*"));
+console.log(arrayMethodJoin);
+Array.prototype.join = ArrayObject.join;
+console.log(arrayMethodJoin.join("***"));
+console.log(arrayMethodJoin);
+
+console.log("Method Reverse");
+console.log(ArrayObject.reverse.call(arrayMethodReverse));
+console.log(arrayMethodReverse);
+Array.prototype.reverse = ArrayObject.reverse;
+console.log(arrayMethodReverse.reverse());
+console.log(arrayMethodReverse);
