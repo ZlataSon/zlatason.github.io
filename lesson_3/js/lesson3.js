ea9a3c4de436f5d03f0cb387654020c3057493a3
Zlata Son <filingame12@gmail.com>
2016-10-30 13:33:48 +0200

    commit lesson 3 factorial

--- lesson_3/js/lesson3.js
+++ lesson_3/js/lesson3.js
@@ -1,6 +1,19 @@
 
-function factorial(n)
-{
+// Task 1.2. Ступінь числа рекурсивно
+function stepen(x, n) {
+    if (n != 1) {
+        return x * stepen(x, n - 1);
+    } else {
+        return x;
+    }
+}
+
+console.log("xn = x * xn-1 with Recursion");
+console.log( pow(2, 3) ); // 8
+console.log( pow(5, 2) );
+
+// Task 2.1 Факторіал без рекурсії
+function factorial(n) {
     var z = 1;
     for (i = 1; i <= n; i++) z = z * i;
     return (z);
@@ -8,3 +21,4 @@ function factorial(n)
 
 console.log("Factorial without Recursion");
 console.log(factorial(10));
+console.log(factorial(5));
