382039d11fb35b633bc43bbaa17332fd0ee10322
ZlataSon <filingame12@gmail.com>
2016-12-06 01:10:54 +0200

    Zlata LESSON 7 jQuery

--- lesson_7/js/lesson7.js
+++ lesson_7/js/lesson7.js
@@ -1,1 +1,37 @@
 
+// scripts .js
+
+$(function () {
+
+    $('#todoList ul').sortable({
+
+        items: "li:not('.listTitle, .addItem')",
+        connectWith:"ul",
+        dropOnEmpty:true,
+        placeholder:"emptySpace"
+
+    });
+
+    $('input').keydown(function(e) {
+
+        if (e.keyCode == 13) {
+
+            var item = $(this).val();
+
+            $(this).parent().parent().append('<li id="Bullet">' + item + '</li>');
+            $(this).val('');
+        }
+
+    });
+
+    $('#trash').droppable({
+
+        drop:function(event,ui) {
+
+            ui.draggable.remove();
+
+        }
+
+    });
+
+});
