fb761e6072e9686148d72740b72f4018565cb1bd
ZlataSon <filingame12@gmail.com>
2016-12-18 18:07:21 +0200

    Zlata lesson 9

--- lesson_9/js/lesson9.js
+++ lesson_9/js/lesson9.js
@@ -1,52 +1,72 @@
-var map;
+// TASK 2 FREE API Love Calc
 
-function initialize() {
-    //создаем объект для работы с картой
-    map = new GMap2(document.getElementById("map_canvas"));
-    if (GBrowserIsCompatible()) {
-        //устанавливаем координаты и начальное приближение
-        map.setCenter(new GLatLng(50.453629, 30.502838), 13);
-        //добавляем шкалу зума
-        map.addControl(new GLargeMapControl());
-        //добавляем переключатель типа карт (Карта, Спутник, Гибрид)
-        map.addControl(new GMapTypeControl());
-        //указываем тип карты по-умолчанию (Спутник)
-        map.setMapType(G_SATELLITE_MAP);
-    }
-}
+$('#loveBTN').click(function () {
+    doItLove();
+});
 
-var geocoder = new GClientGeocoder();
-
-function getAdress() {
-    var address = document.getElementById("sityname").value;
-    geocoder.getLatLng(
-        address,
-        function(point) {
-            if (!point) {
-                alert(address + " не найден");
-            } else {
-                geocoder.getLocations(address, addAdr);
-            }
+function doItLove() {
+    var name1 = $('#name1').val();
+    var name2 = $('#name2').val();
+    var output = $.ajax({
+        method: 'GET',
+        cache: false,
+        url: 'https://love-calculator.p.mashape.com/getPercentage', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {
+            fname: name1,
+            sname: name2 }, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            //
+            //Change data.source to data.something , where something is whichever part of the object you want returned.
+            //To see the whole object you can output it to your browser console using:
+            //console.log(data);
+            document.getElementById("loveResult").innerHTML = data.percentage + "%" + ' ' + data.result;
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "4nTQcgjCA9mshfJmLrEJXYUd4cSGp1Fpo3djsnXO7uLmJnw36m"); // Enter here your Mashape key
         }
-    );
+    });
+
 }
 
-function addAdr(response) {
-    //удаляем слои, если они есть
-    map.clearOverlays();
-    if (!response || response.Status.code != 200) {
-        alert("\"" + address + "\" не найден");
-    } else {
-        //создаем объект типа GLatLng и надпись
-        place = response.Placemark[0];
-        point = new GLatLng(place.Point.coordinates[1],
-            place.Point.coordinates[0]);
-        marker = new GMarker(point);
-        //размещаем надпись на карте
-        map.addOverlay(marker);
-        //добавляем текст на надпись
-        marker.openInfoWindowHtml(place.address + '<br />' +
-            'Широта: ' + place.Point.coordinates[1] + '<br />' +
-            'Долгота: ' + place.Point.coordinates[0]);
-    }
+
+// Task 3
+
+
+$('#weatherBTN').click(function () {
+    doItWeather();
+});
+
+function doItWeather() {
+    var lat = $('#lat').val();
+    var lng = $('#lng').val();
+    var output = $.ajax({
+        method: 'GET',
+        cache: false,
+        url: 'https://simple-weather.p.mashape.com/weatherdata', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {
+            lat: lat,
+            lng: lng }, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            //
+            //Change data.source to data.something , where something is whichever part of the object you want returned.
+            //To see the whole object you can output it to your browser console using:
+            //console.log(data);
+            document.getElementById("weatherResult").innerHTML = data.query.results.channel.lastBuildDate + "<br/>"
+                + data.query.results.channel.description + "<br/>" + "<img src='"+data.query.results.channel.image.url+"'/>" + "<br/>"
+                + "Today: temperature" + " " + data.query.results.channel.item.condition.temp + " " + "C" + ", " + data.query.results.channel.item.condition.text + "<br/>"
+                + "Sunrise: " + data.query.results.channel.astronomy.sunrise + ", " + "sunset: " + data.query.results.channel.astronomy.sunset;
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "4nTQcgjCA9mshfJmLrEJXYUd4cSGp1Fpo3djsnXO7uLmJnw36m"); // Enter here your Mashape key
+        }
+    });
+
 }
+
+
---
+++ lesson_9/js/lesson9_map.js
@@ -0,0 +1,73 @@
+//TASK 1 Google Map API search
+
+function initAutocomplete() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 49.444431, lng: 32.059769},
+        zoom: 13,
+        mapTypeId: 'roadmap'
+    });
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById('pac-input');
+    var searchBox = new google.maps.places.SearchBox(input);
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+    });
+
+    var markers = [];
+    // Listen for the event fired when the user selects a prediction and retrieve
+    // more details for that place.
+    searchBox.addListener('places_changed', function() {
+        var places = searchBox.getPlaces();
+
+        if (places.length == 0) {
+            return;
+        }
+
+        // Clear out the old markers.
+        markers.forEach(function(marker) {
+            marker.setMap(null);
+        });
+        markers = [];
+
+        // For each place, get the icon, name and location.
+        var bounds = new google.maps.LatLngBounds();
+        places.forEach(function(place) {
+            if (!place.geometry) {
+                console.log("Returned place contains no geometry");
+                return;
+            }
+            var icon = {
+                url: place.icon,
+                size: new google.maps.Size(71, 71),
+                origin: new google.maps.Point(0, 0),
+                anchor: new google.maps.Point(17, 34),
+                scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            markers.push(new google.maps.Marker({
+                map: map,
+                icon: icon,
+                title: place.name,
+                position: place.geometry.location,
+                lat: place.geometry.location.lat(),
+                lng: place.geometry.location.lng()
+            }));
+
+            console.log("Lat: " + place.geometry.location.lat());
+            console.log("Lng: " + place.geometry.location.lng());
+
+            if (place.geometry.viewport) {
+                // Only geocodes have viewport.
+                bounds.union(place.geometry.viewport);
+            } else {
+                bounds.extend(place.geometry.location);
+            }
+        });
+        map.fitBounds(bounds);
+    });
+}
