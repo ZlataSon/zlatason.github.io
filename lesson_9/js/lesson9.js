a404a0ff1870564ffef47dc816569f8f71427e44
ZlataSon <filingame12@gmail.com>
2016-12-18 18:16:16 +0200

    Zlata lesson 9

--- lesson_9/js/lesson9.js
+++ lesson_9/js/lesson9.js
@@ -1,38 +1,4 @@
-// TASK 2 FREE API Love Calc
-
-$('#loveBTN').click(function () {
-    doItLove();
-});
-
-function doItLove() {
-    var name1 = $('#name1').val();
-    var name2 = $('#name2').val();
-    var output = $.ajax({
-        method: 'GET',
-        cache: false,
-        url: 'https://love-calculator.p.mashape.com/getPercentage', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
-        data: {
-            fname: name1,
-            sname: name2 }, // Additional parameters here
-        dataType: 'json',
-        success: function(data) {
-            //
-            //Change data.source to data.something , where something is whichever part of the object you want returned.
-            //To see the whole object you can output it to your browser console using:
-            //console.log(data);
-            document.getElementById("loveResult").innerHTML = data.percentage + "%" + ' ' + data.result;
-        },
-        error: function(err) { alert(err); },
-        beforeSend: function(xhr) {
-            xhr.setRequestHeader("X-Mashape-Authorization", "4nTQcgjCA9mshfJmLrEJXYUd4cSGp1Fpo3djsnXO7uLmJnw36m"); // Enter here your Mashape key
-        }
-    });
-
-}
-
-
-// Task 3
+// TASK 2 FREE API Weather
 
 
 $('#weatherBTN').click(function () {
@@ -68,5 +34,39 @@ function doItWeather() {
     });
 
 }
+
+// TASK 3 FREE API Love Calc
+
+$('#loveBTN').click(function () {
+    doItLove();
+});
+
+function doItLove() {
+    var name1 = $('#name1').val();
+    var name2 = $('#name2').val();
+    var output = $.ajax({
+        method: 'GET',
+        cache: false,
+        url: 'https://love-calculator.p.mashape.com/getPercentage', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
+        type: 'GET', // The HTTP Method, can be GET POST PUT DELETE etc
+        data: {
+            fname: name1,
+            sname: name2 }, // Additional parameters here
+        dataType: 'json',
+        success: function(data) {
+            //
+            //Change data.source to data.something , where something is whichever part of the object you want returned.
+            //To see the whole object you can output it to your browser console using:
+            //console.log(data);
+            document.getElementById("loveResult").innerHTML = data.percentage + "%" + ' ' + data.result;
+        },
+        error: function(err) { alert(err); },
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader("X-Mashape-Authorization", "4nTQcgjCA9mshfJmLrEJXYUd4cSGp1Fpo3djsnXO7uLmJnw36m"); // Enter here your Mashape key
+        }
+    });
+
+}
+
 
 
